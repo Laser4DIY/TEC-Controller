64875ca23ab988f50fd15fe5a8628dd948f7e8ac
Andreas Kahler <mail@andreaskahler.com>
2018-11-09 21:32:08 +0100

    added licensing info

--- Firmware/TEC-Controller-SW.ino
+++ Firmware/TEC-Controller-SW.ino
@@ -1,8 +1,29 @@
-/* Arduino based TEC Controller
-by Tobias Gutmann 2018
-based on http://hololaser.kwaoo.me/electronics/Arduino_TEC/Arduino-TEC.html
+/* 
+    Arduino based TEC Controller
+    by Tobias Gutmann 2018
+    part of the LASER4DIY project (laser4diy.org)
+    based on http://hololaser.kwaoo.me/electronics/Arduino_TEC/Arduino-TEC.html
 
---- HARDEWARE CONFIGS: ---
+    Copyright FabLab Muenchen e.V. 2018.    
+*/
+
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+    HARDEWARE CONFIGS: 
 
     JUMPER SETTINGS (defines which is the top or bottom board):
     
